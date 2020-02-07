4a5d474999d74c8c16965be604f391e5e1b0e57b
Junior Ricardo <neo.junior.16@gmail.com>
2020-02-07 13:08:49 -0300

    Agregando mas comentarios Comida BE

--- BE/Comida.cs
+++ BE/Comida.cs
@@ -6,6 +6,32 @@ using System.Text;
 
 namespace BE
 {
+    /**
+        Listar todas las columnas
+        var listadoCompleto = db.Tabla.ToList();
+
+        Seleccionar una columna --> Listado de string, porque es el tipo de dato de la propiedad
+        var listaNombre = db.Personas.Select( x => x.Nombre).ToList();
+
+        Seleccionar varias columnas y utilizando un tipo 'anonimo'
+        var listaAnonima = db.Persona.Select( x => new { Nombre = x.Nombre, Edad = x.Edad }).ToList();
+
+        Seleccionar varias columnas  y proyectarlas hacia 'Persona'
+        var listaAnonima = db.Persona.Select( x => new { Nombre = x.Nombre, Edad = x.Edad }).ToList()
+                                            .Select( x => new Persona(){ Nombre = x.Nombre, Edad= x.Edad}).ToList();
+
+
+        Damos a entender a EF que x, ya existe en la base de datos y que le tiene que prestar atencion
+        db.Persona.Attach(x)
+
+        Si la una propiedad tiene 'virtual', se activa la funcionalidad de Lazy Loading, en cual permite, traer los elementos
+        solo si son llamados, ejemplo, traer nuna lista dentro un objeto, esta lista permanecera nula si nunca es llamada.
+        Esta manera de utilizar puede realizar dos Querys.
+
+        'Include' -->  de esta manera realizamos una llamada a la base de datos y tambien trae con ella 'Direcciones', el cual
+                       es una lista. 
+        var persona = db.Persona.Include("Direcciones").FirstOrDefault(x => x.Id == 2);
+    */
     [Table("Comida")]
     public class Comida
     {
